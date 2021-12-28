328215eb190fd557e81341730a629edbd9ba176f
namdlefhtes <namdlefhtes@gmail.com>
2021-12-28 10:53:12 -0500

    ship and gameBoard func

--- ship.js
+++ ship.js
@@ -4,15 +4,31 @@
 // sunk or not sunk
 //https://www.hasbro.com/common/instruct/Battleship.PDF
 
-const ship = (length, hitLocation, status, dmg) => {
+const ship = (length, status, boardLocation) => {
     return {
-        length: length,
-        hitLocation: hitLocation,
-        status: status,
-        hit(dmg) {
-            
-        }
-    };
+        length,
+        status,
+        boardLocation
+    }
+}
+
+const hit = (targ) => {
+    // this needs the gameboard
+    
+    //isSunk function inside to check when BS is sunk.
 };
 
-module.exports = ship;
+
+const gameBoard = (nums, letters) => {
+    // A-G, 1-8
+    // build obj with x(numbers) and y (letters) axis
+    let gameBoardObj = {
+        x:  nums,
+        y: letters    
+    };
+
+    return gameBoardObj;
+}
+
+module.exports.ship = ship;
+module.exports.gameBoard = gameBoard;
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,15 @@
-const ship = require("./ship");
 
-test('test', () =>{
-    expect(ship("5","rear", "OK")).toEqual({"hitLocation": "rear", "length": "5", "status": "OK"});
+const gameFunc = require("./ship");
+const ship = gameFunc.ship;
+const gameBoard = gameFunc.gameBoard;
+
+const xAx = [1,2,3,4,5,6,7,8];
+const yAx = ["A","B", "C", "D", "E", "F", "G"];
+
+test('ship test', () =>{
+    expect(ship("2","OK", "['d6','d7']")).toEqual({"length": "2", "status": "OK", "boardLocation":"['d6','d7']"});
+});
+
+test('gameboard', ()=> {
+    expect(gameBoard(xAx, yAx)).toEqual({"x": xAx, "y": yAx});
 });
