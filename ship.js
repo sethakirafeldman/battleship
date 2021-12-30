bf6feec7beb3345ac43c3e05ba66ea423d66d395
namdlefhtes <namdlefhtes@gmail.com>
2021-12-30 10:58:23 -0500

    trying to get ship obj to mod gameboard

--- ship.js
+++ ship.js
@@ -3,25 +3,14 @@
 // where hit 
 // sunk or not sunk
 //https://www.hasbro.com/common/instruct/Battleship.PDF
+const gameBoardObj = {};
+const ships = [];
 
-const ship = (length, status, boardLocation) => {
-    return {
-        length,
-        status,
-        boardLocation
-    }
-}
-
-const hit = (y, x) => {
-    // this needs the gameboard
-    
-    //isSunk function inside to check when BS is sunk.
-};
-
+const xAx = [1,2,3,4,5,6,7,8];
+const yAx = ["A","B", "C", "D", "E", "F", "G"];
 
 const gameBoard = (nums, letters) => {
     // A-G, 1-8
-    let gameBoardObj = {};
     let boardArr = [];
     for (let i=0; i <= nums.length; i++) {
         for ( let j = 0; j <= letters.length; j++ ) {
@@ -40,5 +29,26 @@ const gameBoard = (nums, letters) => {
     return gameBoardObj;
 }
 
-module.exports.ship = ship;
+gameBoard(xAx,yAx);
+
+const ship = (length, status, boardLocation) => {
+    return {
+        length,
+        status,
+        boardLocation,
+        pushTo: (ship) => {
+            ships.push(ship)
+        } 
+    }
+    console.log(ship);
+    
+}
+
+const hit = (y, x) => {
+    // this needs the gameboard
+    
+    //isSunk function inside to check when BS is sunk.
+};
+
 module.exports.gameBoard = gameBoard;
+module.exports.ship = ship;
