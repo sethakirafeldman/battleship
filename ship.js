006bed79be995895bd9807f40d2ae88b2cb7a9ec
namdlefhtes <namdlefhtes@gmail.com>
2022-01-01 11:04:08 -0500

    trying to store ship data in array.

--- ship.js
+++ ship.js
@@ -9,46 +9,48 @@ const ships = [];
 const xAx = [1,2,3,4,5,6,7,8];
 const yAx = ["A","B", "C", "D", "E", "F", "G"];
 
-const gameBoard = (nums, letters) => {
-    // A-G, 1-8
-    let boardArr = [];
-    for (let i=0; i <= nums.length; i++) {
-        for ( let j = 0; j <= letters.length; j++ ) {
-            let letNum = letters[i]+nums[j];
-            boardArr.push(letNum);
-        }    
+const gameBoard = {
+    board: (nums, letters) => {
+      // A-G, 1-8
+        let boardArr = [];
+        for (let i=0; i <= nums.length; i++) {
+            for ( let j = 0; j <= letters.length; j++ ) {
+                let letNum = letters[i]+nums[j];
+                boardArr.push(letNum);
+            }    
     };
-
-    for (let k=0; k < letters.length * nums.length; k++) {  
-        gameBoardObj[boardArr[k]] = {
-            controlled: "no",
-            type: "open"    
+        for (let k=0; k < letters.length * nums.length; k++) {  
+            gameBoardObj[boardArr[k]] = {
+                controlled: "no",
+                type: "open"    
+            };
         };
-    };
+        return gameBoardObj;
+        },
 
-    return gameBoardObj;
-}
+    ship: (length, status, boardLocation, pushLoc) => {
+        return {
+            length,
+            status,
+            boardLocation,
+            pushLoc: ()=> {
+                ships.push(ship); 
+            }      
+           
+        }   
+        
+        console.log(ships);
+    }  
+};
 
-gameBoard(xAx,yAx);
+gameBoard.board(xAx,yAx);
 
-const ship = (length, status, boardLocation) => {
-    return {
-        length,
-        status,
-        boardLocation,
-        pushTo: (ship) => {
-            ships.push(ship)
-        } 
-    }
-    console.log(ship);
-    
-}
-
+gameBoard
 const hit = (y, x) => {
     // this needs the gameboard
     
     //isSunk function inside to check when BS is sunk.
 };
 
-module.exports.gameBoard = gameBoard;
-module.exports.ship = ship;
+module.exports.gameBoard = gameBoard.board;
+module.exports.ship = gameBoard.ship;
--- ship.test.js
+++ ship.test.js
@@ -1,15 +1,15 @@
 
 const gameFunc = require("./ship");
 const ship = gameFunc.ship;
-const gameBoard = gameFunc.gameBoard;
+const board = gameFunc.gameBoard.board;
 
-const xAx = [1,2,3,4,5,6,7,8];
-const yAx = ["A","B", "C", "D", "E", "F", "G"];
+// const xAx = [1,2,3,4,5,6,7,8];
+// const yAx = ["A","B", "C", "D", "E", "F", "G"];
 
 test('ship test', () =>{
     expect(ship("2","OK", "['d6','d7']")).toEqual({"length": "2", "status": "OK", "boardLocation":"['d6','d7']"});
 });
 
-test('gameboard', ()=> {
-    expect(gameBoard(xAx, yAx)).toEqual({"x": xAx, "y": yAx});
-});
+// test('board test', ()=> {
+//     expect(board(xAx, yAx)).toEqual({"x": xAx, "y": yAx});
+// });
